22279bb985b265de0f3c3263047abadcf26384ff
Will Christen <christenwill58@gmail.com>
2022-06-06 19:55:03 -0400

    Part 2 Complete

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener('load', function(){
+    takeOff = document.getElementById('takeoff');
+    takeOff.addEventListener('click', function(){
+        let takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (takeOffConfirmation === true) {
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+            document.getElementById("spaceShuttleHeight").innerHTML = 10000;
+
+        }
+    })
+})
