8ea80c694cc6b669201e32b27314732242d43340
Will Christen <christenwill58@gmail.com>
2022-06-06 20:21:51 -0400

    Part 4 Complete

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
     takeOff.addEventListener('click', function(){
         let takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (takeOffConfirmation === true) {
-            document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
             document.getElementById("shuttleBackground").style.backgroundColor = "blue";
             let height = document.getElementById("spaceShuttleHeight").innerHTML;
             height = Number(height);
@@ -15,8 +15,17 @@ window.addEventListener('load', function(){
     land = document.getElementById("landing");
     land.addEventListener('click', function(){
         window.alert("The shuttle is landing. Landing gear engaged");
-        document.getElementById("flightStatus").innerHTML = "The shuttle has landed";
+        document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
         document.getElementById("shuttleBackground").style.backgroundColor = "green";
         document.getElementById("spaceShuttleHeight").innerHTML = "0";
+    })
+    abort = document.getElementById("missionAbort");
+    abort.addEventListener('click', function(){
+        let abortConfirmed = window.confirm("Confirm that you want to abort the mission");
+        if (abortConfirmed === true){
+            document.getElementById("flightStatus").innerHTML = "Mission aborted.";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = "0";
+        }
     })
 })
